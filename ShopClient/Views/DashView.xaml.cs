3f827ee64a8ea23f5ebe924cefa6f43f15d62de1
DeVice233 <yakovenko182003@gmail.com>
2022-03-24 22:27:55 +1000

    AddProductChart

--- ShopClient/Views/DashView.xaml.cs
+++ ShopClient/Views/DashView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopClient.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace ShopClient.Views
         public DashView()
         {
             InitializeComponent();
+            DataContext = new DashViewModel();
         }
     }
 }
